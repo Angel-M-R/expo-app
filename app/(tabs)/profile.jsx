e7d08cc417662a29fb1422e192ce3adf54d8f1bb
Angel-M-R <angelmorenoromera@gmail.com>
2024-05-16 12:07:55 +0200

    feat: 🎸 login form

---
+++ app/(tabs)/profile.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  )
+}
+export default Profile
+const styles = StyleSheet.create({})
